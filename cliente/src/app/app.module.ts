c8bb6f6463442e89cd5ab491df65b4d686208432
Larissa Martins <larissamartins.job@gmail.com>
2021-12-06 11:31:52 -0400

    ATP 77

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListagemClienteComponent } from './listagem-cliente/listagem-cliente.component';
+import { ClienteFormComponent } from './cliente-form/cliente-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListagemClienteComponent
+    ListagemClienteComponent,
+    ClienteFormComponent
   ],
   imports: [
     BrowserModule,
